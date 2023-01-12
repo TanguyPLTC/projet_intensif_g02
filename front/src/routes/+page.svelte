dcf9d3c5f2dfc37b6c091db220550bb6526d7f31
Tanguy Plantecoste <tanguybp@hotmail.fr>
2023-01-12 09:09:09 +0100

    Map

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,95 @@
+<script lang="ts">
+  import * as L from 'leaflet';
+  // If you're playing with this in the Svelte REPL, import the CSS using the
+  // syntax in svelte:head instead. For normal development, this is better.
+  import 'leaflet/dist/leaflet.css';
+
+  /** @type {import('./$types').PageData} */  
+  export let data;
+
+  let map: L.Map;
+
+  type building = {
+    idBuilding: number;
+    adress: string;
+    postalCode: string;
+    city: string;
+    latitude: string;
+    longitude: string;
+    maxPlace: number;
+  };
+
+  const buildingList:building[] = data.item;
+
+  function averageCoords() {
+    let value:[number, number]
+    var sum_latitude = 0;
+    var sum_longitude = 0;
+    for(let build of buildingList) {
+      sum_latitude += +build.latitude;
+      sum_longitude += +build.longitude;
+    };
+    value = [sum_latitude/buildingList.length, sum_longitude/buildingList.length];
+    return value;
+  }
+
+  function createMap(container: string | HTMLElement) {
+    let m = L.map(container).setView(averageCoords(), 13);
+    L.tileLayer(
+      'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+      {
+        attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,
+          &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`,
+        subdomains: 'abcd',
+        maxZoom: 19,
+      }
+    ).addTo(m);
+
+    return m;
+  }
+
+  function mapAction(container: string | HTMLElement) {
+    map = createMap(container);
+    loadMarker();
+    return {
+      destroy: () => {
+        map.remove();
+      },
+    };
+  }
+
+  
+
+  function resizeMap() {
+    if(map) { map.invalidateSize(); }
+  }
+
+  function loadMarker() {
+    for(let build of buildingList) {
+      var marker = L.marker([+build.latitude, +build.longitude]).addTo(map);
+      marker.bindPopup(
+        `<b>${build.adress}</b>
+        <br>${build.city}, ${build.postalCode}</br>
+        <b>Places disponibles: ${build.maxPlace}</b>
+        <Button class="btn btn-primary popupButton">Réserver</Button>`
+      ).on("popupopen", (a) => {
+        var popUp = a.target.getPopup()
+        popUp.getElement()
+        .querySelector(".popupButton")
+        .addEventListener("click", e => {book(build.idBuilding);});
+      }
+      );
+    }  
+  }
+
+  function book(id:number) {
+    //TODO: post reservation
+    console.log("Booking building " + id);
+  }
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <p>{JSON.stringify(buildingList)}</p> -->
+<div style="height:400px;width:100%" use:mapAction />
+<svelte:window on:resize={resizeMap} />
