2392e6debc6be290912c8e8114e5dbcf2547ce2d
Antonin Huaut <me@antoninhuaut.fr>
2023-01-12 09:56:48 +0100

    wip resac

--- front/src/routes/reservation/store.js
+++ front/src/routes/reservation/store.js
@@ -3,6 +3,6 @@ import { writable } from 'svelte/store';
 export const reservation = writable({
     city: "Caen",
     needPlace: 4,
-    dateStart: new Date(),
-    dateEnd: new Date(),
+    dateStart: new Date().toISOString().slice(0, 16),
+    dateEnd: new Date().toISOString().slice(0, 16),
 });
